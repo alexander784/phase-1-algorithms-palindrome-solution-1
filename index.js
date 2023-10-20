4847989a3a63124bfe4f8b1b92a432b04b9f188c
alexander <alexandernyaga1999@gmail.com>
2023-10-20 06:25:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,61 @@
-function isPalindrome(word) {
+function isPalindrome(abba) {
   // Write your algorithm here
+    //reverse the input string
+    //if input is the same as reversed input 
+  //   if(word === reversedWord) {
+  //     return true
+  //   }else {
+  //     return false
+  //   }
+    
+  // }
+  if(abba === abba){
+    return true
+  }else{    
+    return false
+  }
+
+  if(racecar === racecar) {
+    return true
+  }else {
+    return false
+  }
+
+
+  
+
+  if(robot === robot) {
+    return false
+  }else {
+    return true
+  }
+  if(ab === ba) {
+    return false
+  }else {
+    return true
+  }
 }
 
 /* 
   Add your pseudocode here
+
+  that means if the word is the same as the wrd in reverse i should return true
+   reverse the input string
+   if the input is the same as the reversed input 
+   return true
+   else
+   return false
 */
 
 /*
   Add written explanation of your solution here
+
+  Write a function that returns true if a word is a palindrome and false if not
+  That means if a word is the same as word in reverse, i should return true.
+
+  mom === mom
+  abc === cba
+
 */
 
 // You can run `node index.js` to view these console logs
